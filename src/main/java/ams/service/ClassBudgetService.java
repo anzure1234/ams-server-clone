d14c06dd58ccabd2ee11a734cd34fe74c6ef8af7
DESKTOP-4SEQQKB\Admin <anzure1234@gmail.com>
2023-05-23 20:47:35 +0700

    commit

--- src/main/java/ams/service/ClassBudgetService.java
+++ src/main/java/ams/service/ClassBudgetService.java
@@ -5,11 +5,6 @@ import ams.model.entity.ClassBudget;
 import java.util.Optional;
 
 public interface ClassBudgetService extends BaseService<ClassBudget,Long>{
-////    ClassBudget create(ClassBudget classBudget);
-//
-////    ClassBudget update(ClassBudget classBudget);
-//
-//    Optional<ClassBudget> findById(Long id);
 
 
 }
